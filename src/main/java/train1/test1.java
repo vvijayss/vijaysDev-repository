1f58054a9ce72343dc403aa8636e37f51d911fac
vijaybabu <vijaya.babut@wipro.com>
2013-04-01 16:14:18 +0530

    done

--- src/main/java/train1/test1.java
+++ src/main/java/train1/test1.java
@@ -8,6 +8,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testing1 mvn project for bambo build");
+		System.out.println("testing1 mvn project for bambo build-22222");
 
 	}
 
